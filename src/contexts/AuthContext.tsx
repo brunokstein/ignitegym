9ac3969831ee5d8abed9f4c0f310123f6dc29e70
Bruno Kempfer Stein <brunokempferstein@gmail.com>
2023-01-24 13:27:24 -0300

    first commit

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,116 @@
+import { createContext, ReactNode, useState, useEffect } from 'react';
+
+import { storageAuthTokenSave, storageAuthTokenGet, storageAuthTokenRemove } from '@storage/storageAuthToken'
+import { storageUserSave, storageUserGet, storageUserRemove } from '@storage/storageUser';
+
+import { api } from '@services/api';
+import { UserDto } from '@dtos/userDTO';
+
+export type AuthContextDataProps = {
+    user: UserDto;
+    updateUserProfile: (userUpdated: UserDto) => Promise<void>;
+    signIn: (email: string, password: string) => Promise<void>;
+    signOut: () => Promise<void>;
+    isLoadingUserStorageData: boolean;
+}
+
+type AuthContextProviderProps = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+
+    const [user, setUser] = useState<UserDto>({} as UserDto);
+    const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
+
+    async function userAndTokenUpdate(userData: UserDto, token: string) {
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        setUser(userData);
+    }
+
+    async function storageUserAndTokenSave(userData: UserDto, token: string) {
+        try {
+            setIsLoadingUserStorageData(true);
+
+            await storageUserSave(userData);
+            await storageAuthTokenSave(token);
+
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+
+    }
+
+    async function signIn(email: string, password: string) {
+        try {
+            const { data } = await api.post('/sessions', { email, password });
+
+            if (data.user && data.token) {
+                await storageUserAndTokenSave(data.user, data.token);
+                await userAndTokenUpdate(data.user, data.token);
+            }
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
+    async function signOut() {
+        try {
+            setIsLoadingUserStorageData(true);
+
+            setUser({} as UserDto);
+            await storageUserRemove();
+            await storageAuthTokenRemove();
+
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
+    async function updateUserProfile(userUpdated: UserDto) {
+        try {
+            setUser(userUpdated);
+            await storageUserSave(userUpdated);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async function loadUserData() {
+        try {
+            setIsLoadingUserStorageData(true);
+
+            const userLogged = await storageUserGet();
+            const token = await storageAuthTokenGet();
+
+            if (token && userLogged) {
+                userAndTokenUpdate(userLogged, token);
+            }
+
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
+    useEffect(() => {
+        loadUserData();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{
+            user, signIn, signOut, isLoadingUserStorageData, updateUserProfile
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
---
+++ src/screens/History.tsx
@@ -0,0 +1,69 @@
+import { useCallback, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { Heading, VStack, SectionList, Text, useToast } from 'native-base';
+
+import { api } from '@services/api';
+import { HistoryByDayDTO } from '@dtos/HistoryByDayDTO';
+
+import { AppError } from '@utils/AppError';
+import { HistoryCard } from '@components/HistoryCard';
+import { ScreenHeader } from '@components/ScreenHeader';
+
+export function History() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [exercises, setExercises] = useState<HistoryByDayDTO[]>([]);
+
+    const toast = useToast();
+
+    async function fetchHistory() {
+        try {
+            setIsLoading(true);
+            const response = await api.get('/history');
+            setExercises(response.data);
+
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Nao foi possivel carregar o historico.';
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useFocusEffect(useCallback(() => {
+        fetchHistory();
+    }, []));
+
+
+    return (
+        <VStack flex={1}>
+            <ScreenHeader title="Historico de Exercicios" />
+
+            <SectionList
+                sections={exercises}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => <HistoryCard data={item} />}
+                renderSectionHeader={({ section }) => (
+                    <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">
+                        {section.title}
+                    </Heading>
+                )}
+                px={8}
+                contentContainerStyle={[].length === 0 && { flex: 1, justifyContent: 'center' }}
+                ListEmptyComponent={() => (
+                    <Text color="gray.100" textAlign="center">
+                        Nao ha exercicios registrados ainda. {'\n'}
+                        Vamos fazer exercicios hoje?                       `
+                    </Text>
+                )}
+                showsVerticalScrollIndicator={false}
+            />
+
+        </VStack>
+    );
+}
